c82b4105397243eaa1a0d1702461957e07ad8fe4
unknown <tsdcanavan@yahoo.com>
2017-08-26 10:07:53 -0400

    Update to add the returned gifs

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,26 +1,42 @@
 var urlrequest;
 var addHtml;
 var imagesrc;
+var searchWord;
+var offset;
 
-urlrequest = "http://api.giphy.com/v1/gifs/search?api_key=cfc1851d752e4fffb7e72a7e3e916bb1&q=colors&limit=10&offset=0&rating=G&lang=en"
+
+
+  $("#add-search").on("click", function(event) {
+    event.preventDefault();
+
+	offset=Math.round(Math.random() * 1000);
+	searchWord = $("#giphy-input").val().trim();
+	$("#giphy-input").val("");
+	urlrequest = "http://api.giphy.com/v1/gifs/search?" + 
+				"api_key=cfc1851d752e4fffb7e72a7e3e916bb1" +
+				"&q=" + searchWord + "&limit=10&offset=" + offset +
+				 "&rating=G&lang=en";
 
 $.ajax({url:urlrequest,method:"get"}).done(function(response) {
-console.log(response.data[1].images.fixed_width_small_still.url)
 	  var imgOut=$("<img>");
 	  imgOut.attr("class","img-responsive portfolio-item");
-	  imgOut.attr("src","response.data[1].images.fixed_width_small_still.url");
-//	  addHtml.text("<img class="img-responsive portfolio-item" src="http://placehold.it/200x135" alt="">")
+	  imgOut.attr("src",response.data[0].images.fixed_width_small_still.url);
+	  imgOut.attr("alt","giphy search word colors")
 	  $("#image1").html(imgOut);
-//	  imagesrc=data[1].images.fixed_width_small_still.url;
-//	  console.log(imagesrc);
-//      addHtml=$("<img>");
-//  	  addHtml.attr("src",imagesrc);
-//  	  $("#image2").html(addHtml);
-//	  imagesrc=data[2].images.fixed_width_small_still.url;
-//	  console.log(imagesrc);
-//      addHtml=$("<img>");
-//  	  addHtml.attr("src",imagesrc);
-//  	  $("#image3").html(addHtml);
+
+	  var imgOut=$("<img>");
+	  imgOut.attr("class","img-responsive portfolio-item");
+	  imgOut.attr("src",response.data[1].images.fixed_width_small_still.url);
+	  imgOut.attr("alt","giphy search word colors")
+	  $("#image2").html(imgOut);
+
+	  var imgOut=$("<img>");
+	  imgOut.attr("class","img-responsive portfolio-item");
+	  imgOut.attr("src",response.data[2].images.fixed_width_small_still.url);
+	  imgOut.attr("alt","giphy search word colors")
+	  $("#image3").html(imgOut);
 
 	console.log(response);
 });
+
+  });
