4338212c7b7959fd2d3a03709a8fe189c3817697
unknown <tsdcanavan@yahoo.com>
2017-08-28 14:08:39 -0400

    reset after testing rows

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -2,90 +2,90 @@ var urlrequest;
 var searchWord;
 var offset;
 
-$("#reset-search").on("click", function(event) {
-	  var resetDiv=$("<div>");
-	  var resetImg = $("<img>");
+$("#reset-search").on("click", function (event) {
+	var resetDiv = $("<div>");
+	var resetImg = $("<img>");
 
 
-	  // update tags
-	  resetImg.attr("src","https://media.giphy.com/media/3o85xpTdNSwazIr23S/giphy.gif");
-	  resetImg.attr("alt","giphy logo");
-	  resetImg.attr("class","img-responsive gif");
+	// update tags
+	resetImg.attr("src", "https://media.giphy.com/media/3o85xpTdNSwazIr23S/giphy.gif");
+	resetImg.attr("alt", "giphy logo");
+	resetImg.attr("class", "img-responsive gif");
 
-	  resetDiv.append(resetImg);
+	resetDiv.append(resetImg);
 
-      $("#image-out").append(resultDiv);
+	$("#image-out").append(resultDiv);
 
 });
 
-$("#add-search").on("click", function(event) {
-  event.preventDefault();
+$("#add-search").on("click", function (event) {
+	event.preventDefault();
 
-  offset=Math.round(Math.random() * 1000);
-  searchWord = $("#giphy-input").val().trim();
-  if (searchWord !== "") {
-  $("#giphy-input").val("");
-  urlrequest = "https://api.giphy.com/v1/gifs/search?" + 
-				"api_key=cfc1851d752e4fffb7e72a7e3e916bb1" +
-				"&q=" + searchWord + "&limit=9&offset=" + offset +
-				"&rating=G&lang=en";
+	offset = Math.round(Math.random() * 1000);
+	searchWord = $("#giphy-input").val().trim();
+	if (searchWord !== "") {
+		$("#giphy-input").val("");
+		urlrequest = "https://api.giphy.com/v1/gifs/search?" +
+			"api_key=cfc1851d752e4fffb7e72a7e3e916bb1" +
+			"&q=" + searchWord + "&limit=9&offset=" + offset +
+			"&rating=G&lang=en";
 
-  $.ajax({
-  	url:urlrequest,
-  	method:"get"
-  }).done(function(response) {
+		$.ajax({
+			url: urlrequest,
+			method: "get"
+		}).done(function (response) {
 
-	console.log(response);
-    var results=response.data;
+			console.log(response);
+			var results = response.data;
 
-    $("#image-out").empty();
+			$("#image-out").empty();
 
-    // add images
-	for (var i = 0; i < results.length; i++) {
-	  if (((i+1)%3)===0) {
-      $("#image-out").append(resultDiv);
-       }
-
-
-	  // create tags
-	  var resultDiv=$("<div>");
-	  var p = $("<p>");
-	  var resultImg = $("<img>");
+			// add images
+			for (var i = 0; i < results.length; i++) {
+				if (((i + 1) % 3) === 0) {
+					$("#image-out").append(resultDiv);
+				}
 
 
-	  // update tags
-
-	  p.text("Rating: " + results[i].rating);
-	  resultImg.attr("src",results[i].images.fixed_width_still.url);
-	  resultImg.attr("data-still",results[i].images.fixed_width_still.url);
-	  resultImg.attr("data-animate",results[i].images.fixed_width.url);
-	  resultImg.attr("data-state","still");
-	  resultImg.attr("alt",results[i].slug);
-	  resultImg.attr("class","img-responsive portfolio-item gif");
-
-	  resultDiv.append(p);
-	  resultDiv.append(resultImg);
-
-	  }
-
-	console.log("click wait");
-	$(".gif").on("click", function() {
-	  console.log("gif clicked");
-	  var state= $(this).attr("data-state");
-	  if (state==="still") {
-	    $(this).attr("data-state","animate");
-	    $(this).attr("src",$(this).attr("data-animate"));
-	  } else {
-	    $(this).attr("data-state","still");
-	    $(this).attr("src",$(this).attr("data-still"));
-	  }
-	});
+				// create tags
+				var resultDiv = $("<div>");
+				var p = $("<p>");
+				var resultImg = $("<img>");
 
 
-	});
+				// update tags
 
-   }
+				p.text("Rating: " + results[i].rating);
+				resultImg.attr("src", results[i].images.fixed_width_still.url);
+				resultImg.attr("data-still", results[i].images.fixed_width_still.url);
+				resultImg.attr("data-animate", results[i].images.fixed_width.url);
+				resultImg.attr("data-state", "still");
+				resultImg.attr("alt", results[i].slug);
+				resultImg.attr("class", "img-responsive portfolio-item gif");
 
-  });
+				resultDiv.append(p);
+				resultDiv.append(resultImg);
+
+			}
+
+			console.log("click wait");
+			$(".gif").on("click", function () {
+				console.log("gif clicked");
+				var state = $(this).attr("data-state");
+				if (state === "still") {
+					$(this).attr("data-state", "animate");
+					$(this).attr("src", $(this).attr("data-animate"));
+				} else {
+					$(this).attr("data-state", "still");
+					$(this).attr("src", $(this).attr("data-still"));
+				}
+			});
+
+
+		});
+
+	}
+
+});
 
 
