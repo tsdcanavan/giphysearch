723def222aa0384a3ce2248d686636f54776f60b
unknown <tsdcanavan@yahoo.com>
2017-08-30 09:54:14 -0400

    almost complete

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -4,7 +4,7 @@ var offset;
 
 function loadGifs() {
 	offset = Math.round(Math.random() * 1000);
-
+	console.log(searchWord);
 	urlrequest = "https://api.giphy.com/v1/gifs/search?" +
 		"api_key=cfc1851d752e4fffb7e72a7e3e916bb1" +
 		"&q=" + searchWord + "&limit=9&offset=" + offset +
@@ -27,57 +27,46 @@ function loadGifs() {
 			var p = $("<p>");
 			var resultImg = $("<img>");
 
-			var rowNumber = Math.floor((i + 1) % 3);
-			switch (rowNumber) {
-				case 0:
-					resultDiv.attr("class", "col-md-4 portfolio-item");
-					resultDiv.attr("id", "image-row1");
-					p.text("Rating: " + results[i].rating);
-					resultImg.attr("src", results[i].images.fixed_width_still.url);
-					resultImg.attr("data-still", results[i].images.fixed_width_still.url);
-					resultImg.attr("data-animate", results[i].images.fixed_width.url);
-					resultImg.attr("data-state", "still");
-					resultImg.attr("alt", results[i].slug);
-					resultImg.attr("class", "img-responsive portfolio-item gif");
+			resultDiv.attr("class", "col-md-4 portfolio-item");
+			resultDiv.attr("id", "image-id");
+			p.text("Rating: " + results[i].rating);
+			resultImg.attr("src", results[i].images.fixed_width_still.url);
+			resultImg.attr("data-still", results[i].images.fixed_width_still.url);
+			resultImg.attr("data-animate", results[i].images.fixed_width.url);
+			resultImg.attr("data-state", "still");
+			resultImg.attr("alt", results[i].slug);
+			resultImg.attr("class", "img-responsive portfolio-item gif");
 
-					resultDiv.append(p);
-					resultDiv.append(resultImg);
-					$("#image-out").append(resultDiv);
-					break;
-				case 1:
-					resultDiv.attr("class", "col-md-4 portfolio-item");
-					resultDiv.attr("id", "image-row2");
-					p.text("Rating: " + results[i].rating);
-					resultImg.attr("src", results[i].images.fixed_width_still.url);
-					resultImg.attr("data-still", results[i].images.fixed_width_still.url);
-					resultImg.attr("data-animate", results[i].images.fixed_width.url);
-					resultImg.attr("data-state", "still");
-					resultImg.attr("alt", results[i].slug);
-					resultImg.attr("class", "img-responsive portfolio-item gif");
-
-					resultDiv.append(p);
-					resultDiv.append(resultImg);
-					$("#image-out").append(resultDiv);
-					break;
-				case 2:
-					resultDiv.attr("class", "col-md-4 portfolio-item");
-					resultDiv.attr("id", "image-row3");
-					p.text("Rating: " + results[i].rating);
-					resultImg.attr("src", results[i].images.fixed_width_still.url);
-					resultImg.attr("data-still", results[i].images.fixed_width_still.url);
-					resultImg.attr("data-animate", results[i].images.fixed_width.url);
-					resultImg.attr("data-state", "still");
-					resultImg.attr("alt", results[i].slug);
-					resultImg.attr("class", "img-responsive portfolio-item gif");
-
-					resultDiv.append(p);
-					resultDiv.append(resultImg);
-					$("#image-out").append(resultDiv);
-					break;
-			}
-
+			resultDiv.append(p);
+			resultDiv.append(resultImg);
+			$("#image-out").append(resultDiv);
+		}
+	console.log("click wait");
+	$(".gif").on("click", function () {
+		console.log("gif clicked");
+		var state = $(this).attr("data-state");
+		if (state === "still") {
+			$(this).attr("data-state", "animate");
+			$(this).attr("src", $(this).attr("data-animate"));
+		} else {
+			$(this).attr("data-state", "still");
+			$(this).attr("src", $(this).attr("data-still"));
 		}
 	});
+	});
+	// console.log("click wait");
+	// $(".gif").on("click", function () {
+	// 	console.log("gif clicked");
+	// 	var state = $(this).attr("data-state");
+	// 	if (state === "still") {
+	// 		$(this).attr("data-state", "animate");
+	// 		$(this).attr("src", $(this).attr("data-animate"));
+	// 	} else {
+	// 		$(this).attr("data-state", "still");
+	// 		$(this).attr("src", $(this).attr("data-still"));
+	// 	}
+	// });
+
 };
 
 $("#reset-search").on("click", function (event) {
@@ -96,11 +85,6 @@ $("#reset-search").on("click", function (event) {
 
 });
 
-$(".recent-list").on("click", function (event) {
-	searchWord = $("#recent-list").val().trim();
-	console.log(searchWord);
-	loadGifs();
-})
 
 $("#add-search").on("click", function (event) {
 	event.preventDefault();
@@ -112,26 +96,19 @@ $("#add-search").on("click", function (event) {
 		// Add the search to a list of recent searches
 		var addButton = $("<button>");
 		addButton.text(searchWord);
-		addButton.attr("class","recent-list");
+		addButton.attr("class", "recent-list");
 		addButton.attr("value", searchWord);
 		$("#recent").prepend(addButton);
 		loadGifs();
 
-		console.log("click wait");
-		$(".gif").on("click", function () {
-			console.log("gif clicked");
-			var state = $(this).attr("data-state");
-			if (state === "still") {
-				$(this).attr("data-state", "animate");
-				$(this).attr("src", $(this).attr("data-animate"));
-			} else {
-				$(this).attr("data-state", "still");
-				$(this).attr("src", $(this).attr("data-still"));
-			}
-		});
-
-
 	}
+
+	$(".recent-list").on("click", function (event) {
+		console.log("recent click")
+		console.log($(this).attr("value"))
+		searchWord = $(this).attr("value");
+		loadGifs();
+	})
 });
 
 
--- notes.md
+++ notes.md
@@ -1,6 +1,4 @@
 # giphysearch
 .Align so only 3 images are on a line
-.add buttons of recent searches
-.make the recent searches do a re-search
+.When doing recent searches, only do the one clicked
 
-Create a search and display using the giphy api
